d402d2f777f759af4f27b72b7983ec957419562b
yunon <38744862+yunonXD@users.noreply.github.com>
2023-07-26 14:06:36 +0900

    step_2_Scanf_정수실수

--- 2_/printfscanf.c
+++ 2_/printfscanf.c
@@ -2,8 +2,21 @@
 
 int main(void){
 
-    int m_age = 12;
+    //정수형 변수에 대한 예제
+    // int m_age = 12;
+    // printf("%d \n" , m_age);
 
-    printf("%d \n" , m_age);
+    // m_age = 14;
+    // printf("%d \n" , m_age);
+
+
+    //실수형 변수에 대한 예제
+    float m_f = 45.4f;
+    printf("%.2f\n" , m_f);
+
+    double m_d = 4.425;
+    printf("%.2lf" , m_d );
+
+    
     return 0;
 }

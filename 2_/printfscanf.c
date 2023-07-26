2fa0ffda87f7af402d294a2fcf202627aa2165de
yunon <38744862+yunonXD@users.noreply.github.com>
2023-07-26 14:11:10 +0900

    step_2_Scanf_printf

--- 2_/printfscanf.c
+++ 2_/printfscanf.c
@@ -18,11 +18,19 @@ int main(void){
     // printf("%.2lf" , m_d );
 
     //상수. 변하지 않는 수
-    const int Year = 2000;
-    printf("태어난 년도 : %d" , Year);
+    // const int Year = 2000;
+    // printf("태어난 년도 : %d" , Year);
     //Year = 2001;
 
 
+    //printf
+    //연산
+
+    // int add = 3 + 7; // 10
+
+    // printf ( " 3 + 7  = %d\n" , add);
+
+    printf ("%d + %d = %d \n" , 3, 7, 3 + 7);
     
     return 0;
 }

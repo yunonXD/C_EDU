5d3f5178142eacd7f89cb31bb345f7e3fe812738
yunon <38744862+yunonXD@users.noreply.github.com>
2023-07-28 22:39:29 +0900

    step_10_fprintf_fscanf

--- 10_/file.c
+++ 10_/file.c
@@ -22,30 +22,65 @@ int main(void){
 
     // //파일을 열고 닫지 않은 상태에서 
     // //어떤 프로그램 문제로 데이터 손실이 있을 수 있음
+    // fclose(file);   //저장 및 닫기
+
 
     //파일읽기
-    // fclose(file);   //저장 및 닫기
-    char line[MAX];     //char line[10000]
-    FILE *file = fopen("c:\\test1.txt" , "rb");     //r w a / t b
-    if(file == NULL){
-        printf("열기 실패\n");
-        return 1;
-    }
+    // char line[MAX];     //char line[10000]
+    // FILE *file = fopen("c:\\test1.txt" , "rb");     //r w a / t b
+    // if(file == NULL){
+    //     printf("열기 실패\n");
+    //     return 1;
+    // }
 
-    while(fgets(line,MAX,file) != NULL){
-        printf("%s",line);
-    }
+    // while(fgets(line,MAX,file) != NULL){
+    //     printf("%s",line);
+    // }
 
     //파일을 열고 닫지 않은 상태에서 
     //어떤 프로그램 문제로 데이터 손실이 있을 수 있음
-    fclose(file);   //저장 및 닫기
+    //fclose(file);   //저장 및 닫기
 
 
 
     //fprintf fscanf 쌍 
     int num[6] = {0,0,0,0,0,0}; // 추첨번호
+    int bonus = 7 ; //보너스
+    char str1[MAX];
+    char str2[MAX];
+
+    //파일쓰기
+    // FILE *file = fopen("c:\\text2.txt" , "wb");
+    // if(file == NULL){
+    //     printf("파일 열기 실패");
+    //     return 1;
+    // }
+
+    // //로또 추첨
+    // fprintf(file, "%s %d %d %d %d %d %d\n" , "추첨번호", 1,2,3,4,5,6);
+    // fprintf(file, "%s %d\n","보너스 번호", 7);
+
+    //fclose(file);
+
+    //파일 읽기
+    char line[MAX];     //char line[10000]
+    FILE *file = fopen("c:\\text2.txt" , "rb");     //r w a / t b
+    if(file == NULL){
+        printf("열기 실패\n");
+        return 1;
+    }
+
+    fscanf(file,"%s %d %d %d %d %d %d", str1, &num[0], &num[1],
+     &num[2], &num[3], &num[4], &num[5]);
+
+    printf("%s %d %d %d %d %d %d\n",str1,num[0], num[1],
+     num[2], num[3], num[4], num[5]);
+
+    fscanf(file,"%s %d", str2, &bonus);
+    printf("%s %d\n",str2,bonus);
 
 
+    fclose(file);
 
     return 0;
 }

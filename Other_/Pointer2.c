8bad7b72ef847f5bab87a63dd78c8aaeb7b6a1b6
YunonXD <HowLewd@proton.me>
2023-08-05 16:12:36 +0900

    function argument passing

--- Other_/Pointer2.c
+++ Other_/Pointer2.c
@@ -1,20 +1,21 @@
 #include<stdio.h>
 
+void fot(int *ptr);
+
 int main(void){
 
 
-    int num1= 0 , num2 = 20, num3 = 30;
-    int *arr[3] = {&num1,&num2,&num3};
-
-    printf("%d\n", *arr[0]);
-    printf("%d\n", *arr[1]);
-    printf("%d\n", *arr[2]);
-
-    char *strArr[3] = {"Simple" , "String" , "Array"};
-    printf("%s \n", strArr[0]);
-    printf("%s \n", strArr[1]);
-    printf("%s \n", strArr[2]);
+    int age;
+    printf("ют╥б");
+    scanf("%d\n" , &age);
+    fot(&age);
+    printf("%d\n", age);
 
 
     return 0;
+}
+
+void fot(int *ptr){
+
+    *ptr+=1;
 }

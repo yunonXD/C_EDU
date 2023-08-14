b412a15bb351ac0d20276292a08c044de28e6d8b
YunonXD <HowLewd@proton.me>
2023-08-14 16:40:41 +0900

    atoi_atol_atof

---
+++ Other_/atoi_atol_atof.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+
+//int atoi          //문자열 내용을 int 로 변환
+//long atol         //long 으로 변환
+//double atof       //double 로 변환
+
+int main(int argc, char *argv[]){
+
+    char str[20];
+    printf("정수 입력 :");
+    scanf("%s",str);
+    printf("%d \n",atoi(str));
+
+    printf("실수 입력:");
+    scanf("%d",str);
+    printf("%g \n",atof(str));
+    
+
+
+    return 0;
+}

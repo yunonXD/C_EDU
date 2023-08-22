5a6a0381e8760764a2e1218c57e5c40180f646f4
YunonXD <HowLewd@proton.me>
2023-08-22 22:18:37 +0900

    class

---
+++ Class/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+extern void Increment(void);
+extern int GetNum(void);
+
+int main(int argc, char *argv[]){
+
+
+
+    return 0;
+}
---
+++ Class/num.c
@@ -0,0 +1,3 @@
+#include<stdio.h>
+
+int num = 0;

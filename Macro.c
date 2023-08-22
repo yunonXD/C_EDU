748323fd5e39381887133559ea67a1f4fc09c12b
YunonXD <HowLewd@proton.me>
2023-08-22 09:54:36 +0900

    macro2

--- Macro.c
+++ Macro.c
@@ -3,6 +3,7 @@
 
 #define PI 3.14             // 지시자 - 매크로 - 메크로몸체         Object-like macro
 #define SQUARE(X) X*X       // 함수 매크로                         Function like macro
+#define SQUARE2(X) ((X)*(X))    //함수 매크로 문제 생성을 미리 방지
 
 #define NAME                "Yeet"
 #define AGE                 24
@@ -15,7 +16,9 @@ int main(int argc, char *argv[]){
     PRINT_ADDR;
 
 
-    printf("%d" , SQUARE(123));
+    printf("%d \n" , SQUARE(123));      //15129
+    printf("%d \n" , SQUARE(3+2));      //11
+    printf("%d \n", SQUARE2(3+2));      //25
 
 
     return 0;

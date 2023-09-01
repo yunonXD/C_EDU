4024358cf42b3b635c9571bbf6934e3b0667318a
YunonXD <HowLewd@proton.me>
2023-09-01 16:56:23 +0900

    data_structure_23.1_CLinkedList_With_Stack

--- Data_structure/Chapter 06. Stack/LinklistToStack/Solved/CLLBaseStack.h
+++ Data_structure/Chapter 06. Stack/LinklistToStack/Solved/CLLBaseStack.h
@@ -1,6 +1,26 @@
 #ifndef __STACK2_LINKED_LIST_H__
 #define __STACK2_LINKED_LIST_H__
 
+#include"CLinkedList.h"
 
+#define TRUE 1
+#define FALSE 0
+
+//CLinkedList 활용해서 stack 구현하기
+
+typedef int Data;
+
+typedef struct _listStack{
+    List *plist
+}LinkedStack;
+
+typedef LinkedStack Stack;
+
+void StackInit(Stack *pstack);
+int SIsEmpty(Stack *pstack);
+
+void SPush(Stack *pstack , Data data);
+Data SPop(Stack *pstack);
+Data SPeek(Stack *pstack);
 
 #endif

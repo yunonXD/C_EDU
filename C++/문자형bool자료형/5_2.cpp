ca9a0570ce745f4cea542e04db94dba1bde14929
Yunon <lewdmint@protonmail.com>
2023-09-25 22:32:25 +0900

    C++_2_문자형bool자료형

---
+++ C++/문자형bool자료형/5_2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    //char : 작은 문자형
+    char Char_char = 'b';
+    char Char_arr[] = {'a' ,'b' ,'c' ,'\0'};
+
+    cout << Char_char << endl;
+    cout << Char_arr << endl;
+
+    bool isBool = false;
+    bool isBoolTest = 0;
+
+
+    return 0;
+}
--- C++/정수실수형자료형/5_1.cpp
+++ C++/정수실수형자료형/5_1.cpp
@@ -11,9 +11,28 @@ int main(int argc ,char *argv[]){
     long long n_LLONG =LLONG_MAX;
 
     cout << "int n_INT sizeof is : " << sizeof n_INT <<endl;
+    cout << "MAX VALUE :"<< n_INT << "\n" <<endl;
+
     cout << "short n_SHORT sizeof is : " << sizeof n_SHORT <<endl;
+    cout << "MAX VALUE :"<< n_SHORT <<"\n" <<endl;
+
     cout << "long n_LONG is : " << sizeof n_LONG <<endl;
+    cout << "MAX VALUE :"<< n_LONG <<"\n" <<endl;
+
     cout << "long long n_LLONG is : " << sizeof n_LLONG <<endl;
+    cout << "MAX VALUE :"<< n_LLONG <<"\n" <<endl;
+
+    unsigned short b =-2;
+    cout << b << endl;
+    short b2 =-2;
+    cout << b2 << endl;
+
+    cout << "\n" << endl;
+
+    float A1 = 3.14;
+    int B1 = 3.14;
+    cout << A1 << "  " << B1 << endl;
+
 
     return 0;
 }

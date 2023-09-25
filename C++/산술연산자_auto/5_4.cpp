271391f491944181e39983a316e3ec82d8b71265
Yunon <lewdmint@protonmail.com>
2023-09-25 22:59:52 +0900

    C++_4_산술연산자_auto

---
+++ C++/산술연산자_auto/5_4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    int a =10;
+    int b =3;
+
+    int c =a+b;
+    int d =a-b;
+    int e =3*b;
+    int f =a/b;
+    int g =a%b;
+
+    cout << "c:" << c << endl;
+    cout << "d:" << d << endl;
+    cout << "e:" << e << endl;
+    cout << "f:" << f << endl;
+    cout << "g:" << g << endl;
+
+
+    //auto
+    auto n =10;
+    auto x =1.5;
+    auto y =1.3e12l;
+
+
+
+    return 0;
+}

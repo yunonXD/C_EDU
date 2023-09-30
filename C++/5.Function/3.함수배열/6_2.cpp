22a3393c2b07a0ef738d15eacbd5311dd6847038
Yunon <lewdmint@protonmail.com>
2023-09-30 14:57:05 +0900

    C++_15__함수배열

--- C++/5.Function/3.함수배열/6_2.cpp
+++ C++/5.Function/3.함수배열/6_2.cpp
@@ -3,10 +3,25 @@
 
 using namespace std;
 
+const int SIZE = 11;
+
+int sumArr(int[] , int);
 
 int main(int argc, char *atgv[]){
 
-    
+    int arr[SIZE] = {1,2,3,4,5,6,7,8,100,200,300};
+    int sum = sumArr(arr,sizeof(arr)/sizeof(int));
 
+    cout << sum <<endl;
     return 0;
+}
+
+int sumArr(int arr[] , int n){
+    int total =0;
+
+    for(int i=0; i<n; i++){
+        total +=arr[i];
+    }
+
+    return total;
 }
